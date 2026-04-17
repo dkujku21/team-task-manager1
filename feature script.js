1ef643d0ba5168464cb9c7f2f2eaf218c1f9cf37
dkujku21 <korbidalinpf@gmail.com>
2026-04-17 11:50:42 +0200

    Add task input handling in feature script.js

--- feature script.js
+++ feature script.js
@@ -1,1 +1,10 @@
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList = document.getElementById('taskList');
 
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Ju lutem shkruani një detyrë!");
+        return;
